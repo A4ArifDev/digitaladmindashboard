8a8449c65914b70641c4469295345690a2843df2
CodingX <108295338+A4ArifDev@users.noreply.github.com>
2023-05-08 11:46:23 +0600

    remove auth

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './contexts/themeContext';
-import { AuthProvider } from './contexts/AuthContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <AuthProvider>
          <App />
-      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
